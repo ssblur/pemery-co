bd619085ea26b3b44be053de16210c1e173df506
Patrick Emery <patrick.emery@outlook.com>
2020-05-02 11:46:33 -0400

    Added workaround for issue clicking on button icons.
    Signed-off-by: Patrick Emery <patrick.emery@outlook.com>

--- _javascript/main.js
+++ _javascript/main.js
@@ -1,9 +1,30 @@
 'use strict';
 
-$('.more-information').click((e) => {
-    const info = $(e.target).parent().find('.information-card');
-    if(info.hasClass('is-hidden')){
-        $('.information-card').addClass('is-hidden');
-    }
-    info.toggleClass('is-hidden');
+$(() => {
+    const button = $(document.createElement('div'));
+    button.css({
+        margin: 0,
+        padding: 0,
+        width: '100%',
+        height: '100%',
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        zIndex: 100,
+        display: 'none'
+    });
+    button.click((e) => {
+        $(e.target).parent().click()
+    });
+    $('.more-information').each((index, parent) => {
+        $(parent).append(button.clone(true).css('display', 'block'));
+    });
+
+    $('.more-information').click((e) => {
+        const info = $(e.target).parent().find('.information-card');
+        if(info.hasClass('is-hidden')){
+            $('.information-card').addClass('is-hidden');
+        }
+        info.toggleClass('is-hidden');
+    });
 })
